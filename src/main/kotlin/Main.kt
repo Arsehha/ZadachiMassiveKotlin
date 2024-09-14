617a6fc2be71d4b30fbd40b79e4e042b99066a55
Миронов Арсений Кириллович <463-22@std.pkgh.edu>
2024-09-14 17:32:16 +0300

    Zadacha 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,22 @@
-//задача 2
 fun main(){
+    val bukvi = mapOf(21 to 'А', 13 to 'Б', 4 to 'В', 20 to 'Г', 22 to 'Д', 1 to 'Е', 25 to 'Ё', 12 to 'Ж', 24 to 'З',
+        14 to 'И', 2 to 'Й', 28 to 'К', 9 to 'Л', 23 to 'М', 3 to 'Н', 29 to 'О', 6 to 'П', 16 to 'Р', 15 to 'С',
+        11 to 'Т', 26 to 'У', 5 to 'Ф', 30 to 'Х', 27 to 'Ц', 8 to 'Ч', 18 to 'Ш', 7 to 'Щ', 10 to 'Ъ', 17 to 'Ы',
+        19 to 'Ь', 31 to 'Э', 33 to 'Ю', 32 to 'Я',)
+    print("Ключевое слово: ")
+    val keyWord = readln().uppercase()
+    print("Исходный текст: ")
+    val phrase = readln().uppercase()
+    var result = ""
 
-    val arrayM: Array<Array<Int>> = Array(5){
-        Array(5){
-            (1..9).random()
+    for(i in phrase.indices) {
+        val cKey = bukvi.keys.first {k ->
+            bukvi[k] == phrase[i]
         }
-    }
-    print("Массив:")
-    for(i in 0 .. 4){
-        println()
-        for (j in 0 .. 4){
-            print("${arrayM[i][j]} \t")
+        val shift = bukvi.keys.first {k ->
+            bukvi[k] == keyWord[i % keyWord.length]
         }
+        result += bukvi[(cKey + shift) % 33]
     }
-    println()
-
-    var yes=true
-    for (i in 0..4){
-        for (j in 0 .. 4){
-           if(i==j)
-               break
-            if (arrayM[i][j]!=arrayM[j][i]){
-                yes = false
-                break
-            }
-            if(yes==false)
-                break
-        }
-    }
-    if(yes==true){
-        print("Массив симметричен")
-    }
-    else{
-        print("Массив не симметричен")
-    }
+    println(result)
 }
