bb4c19ea2eb07436518ae858418c14842dcc167a
Миронов Арсений Кириллович <463-22@std.pkgh.edu>
2024-09-14 16:48:33 +0300

    Zadacha 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,37 @@
-//1 zadacha
-fun main(args: Array<String>) {
-    print("Введите количество строк: ")
-    val string = readln().toInt()
-    print("Введите количество столбцов: ")
-    val logss = readln().toInt()
+//задача 2
+fun main(){
 
-    val ArrayF: Array<Array<Int>> = Array(string){
-        Array(logss){
-            (100..999).random()
+    val arrayM: Array<Array<Int>> = Array(5){
+        Array(5){
+            (1..9).random()
         }
     }
-    var stringss:String =""
-    for (i in 0 .. string-1) {
-        for (j in 0..logss-1) {
-            stringss += ArrayF[i][j].toString()
-        }
-    }
-
-    for (i in 0 .. string-1) {
+    print("Массив:")
+    for(i in 0 .. 4){
         println()
-        for (j in 0..logss-1) {
-            print(ArrayF[i][j].toString()+"\t")
+        for (j in 0 .. 4){
+            print("${arrayM[i][j]} \t")
         }
     }
     println()
-    println("В массиве использовано ${stringss.toSet().size} различных цифр")
+
+    var yes=true
+    for (i in 0..4){
+        for (j in 0 .. 4){
+           if(i==j)
+               break
+            if (arrayM[i][j]!=arrayM[j][i]){
+                yes = false
+                break
+            }
+            if(yes==false)
+                break
+        }
+    }
+    if(yes==true){
+        print("Массив симметричен")
+    }
+    else{
+        print("Массив не симметричен")
+    }
 }
