be030bac009f8a5a65665929dc0e55c439a2d9ec
Arsehha <159578130+Arsehha@users.noreply.github.com>
2024-09-15 21:14:40 +0300

    zadacha5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,46 @@
 /*
-Создать приложение, в котором пользователь вводит массив из различных слов.
-На выходе приложение должно показать слова сгруппированные по признаку "состоят из одинаковых букв".
-Например, на входе ["eat", "tea", "tan", "ate", "nat", "bat"]. Получаем группы:
-"ate", "eat", "tea"
-"nat", "tan"
-"bat"
-*/
+Создать приложение, в котором пользователь дает на вход два числовых массива, числа в массиве могут повторяться.
+На выходе приложение выдает пересечение этих массивов. Например, на входе [1, 2, 3, 2, 0] и [5, 1, 2, 7, 3, 2, 2].
+На выходе должны получить [1, 2, 2, 3]. То есть учитывается количество повторений чисел в массиве.
+Число 2 в одном массиве повторялось два раза, в другом - три.
+Значит в итоговом массиве число два должно быть два раза*/
 
-fun main() {
-    print("Enter words >>> ")
-    val input = readln().split(' ')
-    val result = mutableMapOf<String, MutableList<String>>()
+fun main(){
+    print("Введите размерность первого массива:")
+    val countMas1 = readln().toInt()
+    print("Введите размерность второго массива:")
+    val countMas2 = readln().toInt()
 
-    for (word in input) {
-        val chars = word.toList().distinct().sorted().toString()
-        val list = result[chars]
-        if (list == null) {
-            result[chars] = mutableListOf(word)
-        }
-        else {
-            list.add(word)
-        }
+    val arrayOne: Array<Int> = Array(countMas1){0}//объявление одномерного массива один
+    val arrayTwo: Array<Int> = Array(countMas2){0}//объявление одномерного массива два
+
+    for(i in 0 ..countMas1-1){
+        print("Первый массив ячейка ${i+1} = ")
+        arrayOne[i]= readln().toInt()
+    }//ручное заполнение массива один
+
+    println()
+    for(i in 0 ..countMas2-1){
+        print("Первый массив ячейка ${i+1} = ")
+        arrayTwo[i]= readln().toInt()
+    }//ручное заполнение массива два
+
+    println()
+    println("Массив один:")
+    for(i in 0 ..countMas1-1){
+        print("${arrayOne[i]} \t")
     }
 
-    for (group in result.values) {
-        for (word in group) {
-            print("$word \t")
-        }
-        println()
+    println()
+    println("Массив два:")
+    for(i in 0 ..countMas2-1){
+        print("${arrayTwo[i]} \t")
     }
+
+    val common = arrayOne.toMutableList()
+    common.retainAll(arrayTwo)
+    common.sort()
+
+    println()
+    println(common)
 }
