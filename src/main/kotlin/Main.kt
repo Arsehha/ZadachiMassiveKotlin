5eb191c4f764701d1c33e930f16cd41076d3920a
Arsehha <159578130+Arsehha@users.noreply.github.com>
2024-09-15 21:35:07 +0300

    zadacha4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,47 @@
-//1 zadacha
-fun main(args: Array<String>) {
-    print("Введите количество строк: ")
-    val string = readln().toInt()
-    print("Введите количество столбцов: ")
-    val logss = readln().toInt()
+/*4 zadacha
+Создать приложение, в котором пользователь дает на вход два числовых массива, числа в массиве могут повторяться.
+На выходе приложение выдает пересечение этих массивов. Например, на входе [1, 2, 3, 2, 0] и [5, 1, 2, 7, 3, 2, 2].
+На выходе должны получить [1, 2, 2, 3]. То есть учитывается количество повторений чисел в массиве.
+Число 2 в одном массиве повторялось два раза, в другом - три.
+Значит в итоговом массиве число два должно быть два раза*/
+fun main() {
+    print("Введите количество элементов первого массива: ")
+    val mas1 = readln().toInt()
+    print("Введите количество элементов второго массива: ")
+    val mas2 = readln().toInt()
 
-    val ArrayF: Array<Array<Int>> = Array(string){
-        Array(logss){
-            (100..999).random()
-        }
-    }
-    var stringss:String =""
-    for (i in 0 .. string-1) {
-        for (j in 0..logss-1) {
-            stringss += ArrayF[i][j].toString()
-        }
-    }
+    var arrayOne:Array<Int> = Array(mas1){0}//инициализация первого массива
+    var arrayTwo:Array<Int> = Array(mas2){0}//инициализация второго массива
 
-    for (i in 0 .. string-1) {
-        println()
-        for (j in 0..logss-1) {
-            print(ArrayF[i][j].toString()+"\t")
-        }
-    }
     println()
-    println("В массиве использовано ${stringss.toSet().size} различных цифр")
+    for(i in 0 .. arrayOne.size-1){
+        print("Введите ${i+1} значение первого массива: ")
+        arrayOne[i]= readln().toInt()
+    }//ручное заполнение первого массива
+
+    println()
+    for(i in 0 .. arrayTwo.size-1){
+        print("Введите ${i+1} значение первого массива: ")
+        arrayTwo[i]= readln().toInt()
+    }//ручное заполнение второго массива
+
+    println()
+    println("Массив один:")
+    for(i in 0 .. arrayOne.size-1){
+        print("${arrayOne[i]} \t")
+    }//вывод первого массива
+
+    println()
+    println("Массив два:")
+    for(i in 0 .. arrayTwo.size-1){
+        print("${arrayTwo[i]} \t")
+    }//вывод второго массива
+
+    var commonElementari = arrayOne.toMutableList()
+    commonElementari.retainAll(arrayTwo)//исключение несовподающих эллементов
+
+    commonElementari.sort()
+
+    println()
+    println(commonElementari)
 }
